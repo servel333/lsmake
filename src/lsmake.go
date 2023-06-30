50c53fd8904d26e777e388414700806d3aaa0b57
Nathaniel Perry <nateperry333@gmail.com>
2023-06-30 04:55:41 -0400

    lsmake.go: Check file exists, better err messages, functions

--- src/lsmake.go
+++ src/lsmake.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"regexp"
 	"sort"
 	"strings"
@@ -21,13 +22,17 @@ func main() {
 	}
 
 	makefile := *makefilePtr
-	targets, err := listTargets(makefile)
-	if err != nil {
-		fmt.Printf("Error reading Makefile: %s\n", err)
+	if !fileExists(makefile) {
+		fmt.Printf("Makefile %s does not exist.\n", makefile)
 		os.Exit(1)
 	}
 
-	fmt.Println("Targets in", makefile+":")
+	targets, err := listTargets(makefile)
+	if err != nil {
+		fmt.Printf("Error reading Makefile %s: %s\n", makefile, err)
+		os.Exit(1)
+	}
+
 	for _, target := range targets {
 		fmt.Println(target)
 	}
@@ -42,36 +47,25 @@ func listTargets(filename string) ([]string, error) {
 
 	scanner := bufio.NewScanner(file)
 	targets := make([]string, 0)
+	seen := make(map[string]bool)
 
 	targetPattern := `(^[^()<>~:;,!?"'*|/]+):`
 	r := regexp.MustCompile(targetPattern)
 
-	seen := make(map[string]bool)
-
 	for scanner.Scan() {
 		line := scanner.Text()
-		matches := r.FindStringSubmatch(line)
-		if len(matches) > 1 {
-			target := matches[1]
-			if target[0] != '.' {
-				if !seen[target] {
-					targets = append(targets, target)
-					seen[target] = true
-				}
+		if target, err := extractTarget(line, r); err == nil && target != "" {
+			if target[0] != '.' && !seen[target] {
+				targets = append(targets, target)
+				seen[target] = true
 			}
-		} else if strings.HasPrefix(line, "include") {
-			includeFile := strings.TrimSpace(strings.TrimPrefix(line, "include"))
-			includeFile = strings.Trim(includeFile, "\"'")
+		} else if includeFile := extractIncludeFile(line); includeFile != "" {
+			includeFile = resolveIncludedFilePath(filename, includeFile)
 			includedTargets, err := listTargets(includeFile)
 			if err != nil {
 				return nil, fmt.Errorf("error processing included file %s: %s", includeFile, err)
 			}
-			for _, includedTarget := range includedTargets {
-				if !seen[includedTarget] {
-					targets = append(targets, includedTarget)
-					seen[includedTarget] = true
-				}
-			}
+			addUniqueTargets(targets, seen, includedTargets)
 		}
 	}
 
@@ -82,6 +76,43 @@ func listTargets(filename string) ([]string, error) {
 	sort.Strings(targets)
 
 	return targets, nil
+}
+
+func extractTarget(line string, r *regexp.Regexp) (string, error) {
+	matches := r.FindStringSubmatch(line)
+	if len(matches) > 1 {
+		return matches[1], nil
+	}
+	return "", fmt.Errorf("no target found in line: %s", line)
+}
+
+func extractIncludeFile(line string) string {
+	includePrefix := "include"
+	if strings.HasPrefix(line, includePrefix) {
+		includeFile := strings.TrimSpace(strings.TrimPrefix(line, includePrefix))
+		includeFile = strings.Trim(includeFile, "\"'")
+		return includeFile
+	}
+	return ""
+}
+
+func resolveIncludedFilePath(baseFile, includeFile string) string {
+	baseDir := filepath.Dir(baseFile)
+	return filepath.Join(baseDir, includeFile)
+}
+
+func addUniqueTargets(targets []string, seen map[string]bool, newTargets []string) {
+	for _, target := range newTargets {
+		if !seen[target] {
+			targets = append(targets, target)
+			seen[target] = true
+		}
+	}
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
 }
 
 func displayUsage() {
